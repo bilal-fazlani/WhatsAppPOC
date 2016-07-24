3ede73de62523863024a6c75140d5364a444c2b7
bilal-fazlani <bilal.m.fazlani@gmail.com>
2016-07-24 13:34:14 +0530

    Added chat messages class and  ichatmanager

---
+++ Whatsapp.ChatManager/ChatMessage.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Whatsapp.ChatManager
+{
+    public class ChatMessage
+    {
+        public string From { get; set; }
+
+        public string Message { get; set; }
+
+        public DateTime TimeStamp { get; set; }
+    }
+}
---
+++ Whatsapp.ChatManager/IChatManager.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Whatsapp.ChatManager
+{
+    public interface IChatManager
+    {
+        Task SaveChat(ChatMessage chatMessage);
+        Task<IEnumerable<ChatMessage>> GetLastMessages(int count);
+        Task<IEnumerable<ChatMessage>> GetAllMessages();
+    }
+}
